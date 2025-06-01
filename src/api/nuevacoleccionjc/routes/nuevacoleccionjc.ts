5ea75a5b2918380ecb832a26cd00466611d65d66
jonatan-c <jona.cc.93@gmail.com>
2025-05-31 22:46:52 -0300

    feat: added news components

---
+++ src/api/nuevacoleccionjc/routes/nuevacoleccionjc.ts
@@ -0,0 +1,7 @@
+/**
+ * nuevacoleccionjc router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::nuevacoleccionjc.nuevacoleccionjc');
---
+++ src/api/nuevacoleccionjc/services/nuevacoleccionjc.ts
@@ -0,0 +1,7 @@
+/**
+ * nuevacoleccionjc service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::nuevacoleccionjc.nuevacoleccionjc');
